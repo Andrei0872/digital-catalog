8d914c02c76872cc4a21e14c1747de6650012234
Andrei Gatej <andreigtj01@gmail.com>
2022-03-31 11:47:10 +0300

    feat: add `Teacher` class

---
+++ src/entities/Teacher.java
@@ -0,0 +1,32 @@
+package entities;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Teacher extends SchoolPerson {
+  private Set<Integer> classesIds = new HashSet<Integer>();
+
+  public Teacher(String name, int age, String email) {
+    super(name, age, email);
+  }
+
+  public void addClassById(int classId) {
+    classesIds.add(classId);
+  }
+
+  public void assignGradeToStudent(int gradeValue, LocalDate gradeDate, Class cls, Student student) throws Exception {
+    boolean teacherHasClass = classesIds.contains(cls.getId());
+    if (!teacherHasClass) {
+      String errMessage = String.format("The teacher(%s) does not have this class(%s)!", this.toString(), cls.toString());
+      throw new Exception(errMessage);
+    }
+
+    if (!cls.hasStudent(student)) {
+      String errMessage = String.format("The student(%s) does not belong to this class(%s)!", student.toString(), cls.toString());
+      throw new Exception(errMessage);
+    }
+
+    student.assignGrade(gradeValue, gradeDate, this.getId(), cls.getId());
+  };
+}

ce10d8b16b0900fb3c4e6b9d40dc6eebcb650b71
Andrei Gatej <andreigtj01@gmail.com>
2022-03-31 12:03:21 +0300

    feat: add the main `School` service

---
+++ src/services/School.java
@@ -0,0 +1,65 @@
+package services;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import entities.Class;
+import entities.Student;
+import entities.Teacher;
+
+public class School {
+  private final String name;
+
+  private Map<Integer, Class> classes = new HashMap<Integer, Class>();
+  private Map<Integer, Teacher> teachers = new HashMap<Integer, Teacher>();
+  private Map<Integer, Student> students = new HashMap<Integer, Student>();
+
+  public School(String name) {
+    this.name = name;
+  }
+
+  public String getName () {
+    return this.name;
+  }
+
+  public void addStudentToClass(Student student, Class cls) {
+    this.ensureClassExists(cls);
+    this.ensureStudentExists(student);
+
+    int studentId = student.getId();
+    cls.addStudentById(studentId);
+  };
+
+  public void addTeacherToClass(Teacher teacher, Class cls) {
+    this.ensureTeacherExists(teacher);
+    this.ensureClassExists(cls);
+
+    teacher.addClassById(cls.getId());
+  };
+
+  private void ensureClassExists (Class cls) {
+    int classId = cls.getId();
+    boolean doesClassExist = classes.containsKey(classId);
+    if (!doesClassExist) {
+      classes.put(cls.getId(), cls);
+    }
+  };
+
+  private void ensureStudentExists (Student student) {
+    int studentId = student.getId();
+    boolean doesStudentExist = students.containsKey(studentId);
+    if (doesStudentExist) {
+      this.students.put(studentId, student);
+    }
+  };
+
+  private void ensureTeacherExists (Teacher teacher) {
+    int teacherId = teacher.getId();
+    boolean doesTeacherExist = teachers.containsKey(teacherId);
+    if (doesTeacherExist) {
+      this.teachers.put(teacherId, teacher);
+    }
+  }
+}

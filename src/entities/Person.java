830ee9e16488ead924f2e481457af43c94a9b111
Andrei Gatej <andreigtj01@gmail.com>
2022-05-19 22:51:09 +0300

    refactor: make `SchoolPerson` have ID and email

--- src/entities/Person.java
+++ src/entities/Person.java
@@ -1,29 +1,19 @@
 package entities;
 
-import utils.IHasIdentifier;
-
-public class Person implements IHasIdentifier {
+public class Person {
   protected final String name;
   protected final int age;
-
-  private static int IDs = 0;
-  private final int ID;
 
   public Person(String name, int age) {
     this.name = name;
     this.age = age;
-
-    IDs++;
-    this.ID = IDs;
   }
 
-  @Override
-  public int getId () {
-    return ID;
+  public String getName () {
+    return name;
   }
 
-  @Override
-  public String toString() {
-    return String.format("Person ID: %s, Name: %s, Age: %s", ID, this.name, this.age);
+  public int getAge () {
+    return age;
   }
 }
--- src/entities/SchoolPerson.java
+++ src/entities/SchoolPerson.java
@@ -1,10 +1,31 @@
 package entities;
 
-public class SchoolPerson extends Person {
-  public final String email;
+import utils.IHasIdentifier;
+
+public class SchoolPerson extends Person implements IHasIdentifier {
+  private final String email;
+  private int ID = -1;
 
   public SchoolPerson(String name, int age, String email) {
     super(name, age);
     this.email = email;
+  }
+
+  public void setId (int ID) {
+    this.ID = ID;
+  }
+
+  @Override
+  public int getId () {
+    return ID;
+  }
+
+  public String getEmail () {
+    return email;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("ID: %s, Name: %s, Age: %s, Email: %s", ID, this.name, this.age, this.email);
   }
 }
